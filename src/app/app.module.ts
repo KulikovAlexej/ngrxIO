a5364cf1e354101ef19ba7655dd4b8b2ddd1f276
TRANSASIA\koolikov.av <koolikov.av@transasia.ru>
2018-12-28 15:18:51 +0300

    удалил все

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,13 @@ import { StoreModule } from '@ngrx/store';
 
 import { AppComponent } from './app.component';
 
-import { MyCounterComponent } from './my-counter/my-counter.component';
-// import { ScoreboardComponent } from './scoreboard/scoreboard.component';
-import { counterReducer } from './ngrx/counter.reducer';
-import { ScoreboardModule } from './modules/scoreboard/scoreboard.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCounterComponent,
-    // ScoreboardComponent
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({
-      counter: counterReducer,
-      // scoreboard: scoreboardReducer
-    }),
-    ScoreboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
